8e43c1bd43621d68ddec35d3c6980269346a46bd
Serah Njoroge <105037236+njoroge-s@users.noreply.github.com>
2024-01-22 14:05:02 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "ClearLink",
+  description: "ClearLink - Talentsync Assesement",
+  icons: {
+    icon: "/favicon.ico"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        
+        {children}
+      
+      </body>
+    </html>
+  )
+}
